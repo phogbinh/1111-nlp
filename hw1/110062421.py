a44267c478040428049777be250638ce0229be5c
Bill Tran <phongbinh2511@gmail.com>
2022-09-19 18:15:57 +0800

    [master] removed unit tests.

--- hw1/110062421.py
+++ hw1/110062421.py
@@ -45,37 +45,6 @@ def edits2(word):
 
 ################ Test Code 
 
-def unit_tests():
-    assert correction('speling') == 'spelling'              # insert
-    assert correction('korrectud') == 'corrected'           # replace 2
-    assert correction('bycycle') == 'bicycle'               # replace
-    assert correction('inconvient') == 'inconvenient'       # insert 2
-    assert correction('arrainged') == 'arranged'            # delete
-    assert correction('peotry') =='poetry'                  # transpose
-    assert correction('peotryy') =='poetry'                 # transpose + delete
-    assert correction('word') == 'word'                     # known
-    assert correction('quintessential') == 'quintessential' # unknown
-    assert words('This is a TEST.') == ['this', 'is', 'a', 'test']
-    assert Counter(words('This is a test. 123; A TEST this is.')) == (
-           Counter({'123': 1, 'a': 2, 'is': 2, 'test': 2, 'this': 2}))
-    assert len(WORDS) == 32198
-    assert sum(WORDS.values()) == 1115585
-    assert WORDS.most_common(10) == [
-     ('the', 79809),
-     ('of', 40024),
-     ('and', 38312),
-     ('to', 28765),
-     ('in', 22023),
-     ('a', 21124),
-     ('that', 12512),
-     ('he', 12401),
-     ('was', 11410),
-     ('it', 10681)]
-    assert WORDS['the'] == 79809
-    assert P('quintessential') == 0
-    assert 0.07 < P('the') < 0.08
-    return 'unit_tests pass'
-
 def spelltest(tests, verbose=False):
     "Run correction(wrong) on all (right, wrong) pairs; report results."
     import time
@@ -101,6 +70,5 @@ def Testset(lines):
             for wrong in wrongs.split()]
 
 if __name__ == '__main__':
-    print(unit_tests())
     spelltest(Testset(open('spell-testset1.txt')))
     spelltest(Testset(open('spell-testset2.txt')))
